f7d78ba51a87d13a1a4239a7b450f4638653ebb1
JOHNDOE <baraka616263@gmail.com>
2024-01-28 17:01:35 +0200

    Initial Commit

---
+++ src/test/java/com/example/JohnCouponPart2/JohnCouponPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JohnCouponPart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JohnCouponPart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
